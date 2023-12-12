e042eb37f84949d70d369439c98edfe0ede46739
nathaliewillemse21 <njpwillemse69@gmail.com>
2023-12-12 16:21:57 +0200

    structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h3>{{ msg }}</h3>
+<!-- <input type="text" v-model="msg"> -->
+  <div  class="wrapper"> 
+  <div class="row" v-if="products.length">
+    <div class="prodCard" v-for="product in products" :key="product.id" >
+      <div class="card-header">
+        <h3>{{ product.prodName }}</h3>
+      </div>
+      <div class="card-body">
+        <img @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" :src="product.image" :alt="product.prodName" loading="lazy">
+        <p :class="product.amount > 420 ? 'expensive' : 'cheap'">R{{ product.amount.toFixed(2) }}</p>
+      </div>
+    </div>
+  </div>
+  <div v-else>
+<h2>Sorry, our products are out of stock.</h2>
+  </div>
+</div>
+     
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      products:[
+        {
+          id: 1,
+          prodName: 'Ichigo Daifuku',
+          category: 'It is filled with a sweet red bean paste and a strawberry',
+          amount: 60,
+          image: 'https://i.postimg.cc/W1mV8yrp/ichigo-daifuku.jpg',
+          
+          
+        },
+        {
+          id: 2,
+          prodName: 'Sakura Mochi',
+          category: 'This classic mochi is wrapped in a pickled sakura leaf',
+          amount: 60,
+          image: 'https://i.postimg.cc/05HyKhGL/sakura-mochi.jpg',
+        },
+        {
+          id: 3,
+          prodName: 'Kusa Mochi',
+          category: 'This mochi is filled with the  sweet red bean paste and give of a hint of green tea.',
+          amount: 75,
+          image: 'https://i.postimg.cc/W4rk5T8F/Kusa-mochi.jpg',
+          
+        },
+      ]
+    }
+  },
+  computed: {
+    categories() {
+      return {
+        msg: 'Sakura Mochi'
+        //   ["Sakura Mochi", "Ichigo Daifuku", "Kusa Mochi"]
+      }
+    }
+
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -22,5 +76,26 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
+}
+
+.wrapper :is(.row) {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.prodCard {
+  width: 18rem;
+}
+.prodCard :is(>*, img){
+  width: 100%;
+  aspect-ratio: 1/1;
+}
+.expensive {
+  font-weight: bolder;
+  color: darkkhaki;
+}
+.cheap {
+  color: black;
 }
 </style>
